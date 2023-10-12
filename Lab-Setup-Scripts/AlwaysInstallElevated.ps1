f11656d5615573d8b94ebad2385bc1dd3aa9bf7d
Nikos Vourdas <nickvourd97@gmail.com>
2023-10-12 12:04:04 +0300

    Add a new line

--- Lab-Setup-Scripts/AlwaysInstallElevated.ps1
+++ Lab-Setup-Scripts/AlwaysInstallElevated.ps1
@@ -25,7 +25,7 @@ New-Item -Path "Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Window
 #Create a new DWORD (32-Bit) Value and insert hex data
 New-ItemProperty -Path "Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Installer" -Name "AlwaysInstallElevated" -PropertyType DWORD -Value 0x00000001
 
-Write-Host "[+] Configuring HKEY_CURRENT_USER registry`n"
+Write-Host "`n[+] Configuring HKEY_CURRENT_USER registry`n"
 #Create a new key in HKEY_CURRENT_USER registry
 New-Item -Path "Registry::\HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Installer"
 
