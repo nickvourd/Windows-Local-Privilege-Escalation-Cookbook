3d77ff5de621ecf0ec0c9797c231fe5422a7c654
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2023-12-21 02:30:51 +0200

    Create EnableLocalAdmin.ps1

---
+++ Lab-Setup-Scripts/EnableLocalAdmin.ps1
@@ -0,0 +1,40 @@
+$global:version = "1.0.0"
+
+$ascii = @"
+
+.____                        .__            .____          ___.     _________       __                
+|    |    ____   ____ _____  |  |           |    |   _____ \_ |__  /   _____/ _____/  |_ __ ________  
+|    |   /  _ \_/ ___\\__  \ |  |    ______ |    |   \__  \ | __ \ \_____  \_/ __ \   __\  |  \____ \ 
+|    |__(  <_> )  \___ / __ \|  |__ /_____/ |    |___ / __ \| \_\ \/        \  ___/|  | |  |  /  |_> >
+|_______ \____/ \___  >____  /____/         |_______ (____  /___  /_______  /\___  >__| |____/|   __/ 
+        \/          \/     \/                       \/    \/    \/        \/     \/           |__|    
+
+~ Created with <3 by @nickvourd
+~ Version: $global:version
+~ Type: EnableLocalAdmin
+
+"@
+
+Write-Host $ascii`n
+
+# Enable the local Administrator account
+Enable-LocalUser -Name "Administrator"
+
+# Set the password for the Administrator account
+$password = ConvertTo-SecureString "Asa31904#!" -AsPlainText -Force
+Set-LocalUser -Name "Administrator" -Password $password
+
+Write-Host "`n[+] Admin Credentials:"
+Write-Host "Username: Administrator"
+Write-Host "Password: Asa31904#!`n"
+
+# Display a message to users
+$msg = "[!] The computer will restart in 20 seconds. Please save your credentials!`n"
+Write-Host $msg
+
+# Wait for 20 seconds
+Start-Sleep -Seconds 20
+
+# Restart the computer
+Restart-Computer -Force
+
