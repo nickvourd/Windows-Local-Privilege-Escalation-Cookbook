4fb7d23559b4356f180311c51b660d5b30c3bafc
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2023-12-26 17:04:22 +0200

    Update StoredCredentialsRunas.ps1

--- Lab-Setup-Scripts/StoredCredentialsRunas.ps1
+++ Lab-Setup-Scripts/StoredCredentialsRunas.ps1
@@ -17,17 +17,5 @@ $ascii = @"
 
 Write-Host $ascii`n
 
-Write-Host "[+] Creating a new user`n"
-#Create new user
-net user nickvourd Passw0rd! /add
-
-Write-Host "[+] Add the new user to Administrators group`n"
-#Add the new user to Administrators Group
-net localgroup "Administrators" nickvourd /add
-
-Write-Host "[+] New user credentials"
-Write-Host "Username: nickvourd"
-Write-Host "Password: Passw0rd!`n"
-
 Write-Host "[+] Use runas to save credentials on the Windows Credentials Manager (Interactive logon)`n"
-runas /savecred /user:WORKGROUP\nickvourd cmd.exe
+runas /savecred /user:WORKGROUP\Administrator cmd.exe
