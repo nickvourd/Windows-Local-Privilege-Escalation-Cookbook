1efd1076fe1b9a9142b9382ed1379e87dd14d335
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2024-01-03 15:10:23 +0200

    Create App1.cs

---
+++ Lab-Setup-Source-Code/App1.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace App1
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is started at: " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 500;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at: " + DateTime.Now);
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e) {
+
+            WriteToFile("Service was run at: " + DateTime.Now);
+
+        }
+
+        public void WriteToFile(string textMsg)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+
+            if (!Directory.Exists(path)){
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\Service1Log_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(textMsg);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(textMsg);
+                }
+            }
+        }
+    }
+}
