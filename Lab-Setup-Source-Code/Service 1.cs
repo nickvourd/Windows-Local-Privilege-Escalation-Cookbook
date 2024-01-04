575e0c6a99fc36ad58bb049c4b92748627222a42
unknown <nickvourd97@gmail.com>
2024-01-04 02:15:44 +0200

    Edit Weak Service Binary

--- Lab-Setup-Source-Code/Service 1.cs
+++ Lab-Setup-Source-Code/Service 1.cs
@@ -11,7 +11,7 @@ using System.Timers;
 using System.IO;
 using System.Runtime.InteropServices.WindowsRuntime;
 
-namespace App2
+namespace Service 1
 {
     public partial class Service1 : ServiceBase
     {
