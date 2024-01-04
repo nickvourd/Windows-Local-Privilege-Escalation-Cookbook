44f0ffcabf5272c17cc33f3879f213ea1b9da0c6
unknown <nickvourd97@gmail.com>
2024-01-04 23:55:45 +0200

    Edit Weak Service Permissions

---
+++ Lab-Setup-Scripts/WeakServicePermissions.ps1
@@ -0,0 +1,44 @@
+$global:version = "1.0.0"
+
+$ascii = @"
+
+.____                        .__            .____          ___.     _________       __                
+|    |    ____   ____ _____  |  |           |    |   _____ \_ |__  /   _____/ _____/  |_ __ ________  
+|    |   /  _ \_/ ___\\__  \ |  |    ______ |    |   \__  \ | __ \ \_____  \_/ __ \   __\  |  \____ \ 
+|    |__(  <_> )  \___ / __ \|  |__ /_____/ |    |___ / __ \| \_\ \/        \  ___/|  | |  |  /  |_> >
+|_______ \____/ \___  >____  /____/         |_______ (____  /___  /_______  /\___  >__| |____/|   __/ 
+        \/          \/     \/                       \/    \/    \/        \/     \/           |__|    
+
+~ Created with <3 by @nickvourd
+~ Version: $global:version
+~ Type: WeakServicePermissions
+
+"@
+
+Write-Host $ascii`n
+
+# Set the path for the folder
+$folderPath = "C:\Program Files\CustomSrv3\"
+
+# Create the folder if it doesn't exist
+if (-not (Test-Path $folderPath)) {
+    New-Item -Path $folderPath -ItemType Directory | Out-Null
+    Write-Host "[+] Folder created successfully at $folderPath`n"
+} else {
+    Write-Host "[+] Folder already exists at $folderPath`n"
+}
+
+Write-Host "[+] Set new file to Service folder`n"
+# Set the URLs of the files to download
+$urlBinary = "https://raw.githubusercontent.com/nickvourd/Windows-Local-Privilege-Escalation-Cookbook/master/Lab-Setup-Binary/Service2.exe"  
+
+# Download index.html
+Invoke-WebRequest -Uri $urlBinary -OutFile "$folderPath\Service3.exe"
+
+Write-Host "[+] Installing the Service3`n"
+# Install the Service3
+New-Service -Name "Vulnerable Service 3" -BinaryPathName "C:\Program Files\CustomSrv3\Service3.exe" -DisplayName "Vuln Service 3" -Description "My Custom Vulnerable Service 3" -StartupType Automatic
+
+Write-Host "[+] Editing the permissions of the Service3"
+# Edit the permissions of the Service3
+cmd.exe /c 'sc sdset "Vulnerable Service 3" D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)(A;;RPWP;;;BU)'
---
+++ Lab-Setup-Source-Code/Service3.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace Service2
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string filename = "exe_files_log.txt";
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string[] exeFiles = Directory.GetFiles(desktopPath, "*.exe");
+
+            // Open file for appending
+            StreamWriter fptr;
+            try
+            {
+                fptr = File.AppendText(filename);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open file: " + e.Message);
+                return;
+            }
+
+            // Get current time
+            DateTime now = DateTime.Now;
+
+            // Write timestamp to the file
+            fptr.WriteLine("\nList of .exe files on the desktop");
+            fptr.WriteLine("Timestamp: " + now.ToString());
+
+            // Write executable file names to the log file
+            foreach (string file in exeFiles)
+            {
+                fptr.WriteLine(file);
+            }
+
+            // Close file
+            fptr.Close();
+
+            Console.WriteLine("Data appended to " + filename);
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+    }
+}
