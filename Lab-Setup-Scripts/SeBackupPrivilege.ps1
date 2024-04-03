ec6779ac506729a91b84f1f48317d37af1be5cad
spyr0 <spyr0sec@protonmail.com>
2024-04-03 09:47:45 +0100

    Add Nuget install and make user creation more PwSh

--- Lab-Setup-Scripts/SeBackupPrivilege.ps1
+++ Lab-Setup-Scripts/SeBackupPrivilege.ps1
@@ -17,24 +17,37 @@ $ascii = @"
 
 Write-Host $ascii`n
 
+$backupUser = "ncv"
+$backupPass = "Passw0rd!"
+
 Write-Host "[+] Creating a new user`n"
-net user ncv Passw0rd! /add
+$SecurePassword = ConvertTo-SecureString -String $backupPass -AsPlainText -Force
+
+$userParams = @{
+    Name = $backupUser
+    Password = $SecurePassword
+    Description = "SeBackupPrivilege User"
+    PasswordNeverExpires = $true
+}
+
+New-LocalUser @userParams
 
 Write-Host "[+] Adding the new user to Remote Management Users group`n"
-net localgroup "Remote Management Users" ncv /add
+Add-LocalGroupMember -Group "Remote Management Users" -Member $backupUser
 
 Write-Host "[+] Enabling WinRM Service`n"
 Enable-PSRemoting -Force
 
 Write-Host "[+] Installing PSPrivilege module`n"
-Install-Module -Name PSPrivilege -Force
+Install-PackageProvider -Name NuGet -Force
+Install-Module -Name PSPrivilege -Force 
 
 Write-Host "[+] Importing PSPrivilege module`n"
 Import-Module PSPrivilege
 
 Write-Host "[+] Granting SeBackupPrivilege to the new user`n"
-Add-WindowsRight -Name SeBackupPrivilege -Account (Get-LocalUser -Name "ncv").Sid
+Add-WindowsRight -Name SeBackupPrivilege -Account (Get-LocalUser -Name $backupUser).Sid
 
 Write-Host "[+] New user's credentials"
-Write-Host "Username: ncv"
-Write-Host "Password: Passw0rd!`n"
+Write-Host "Username: $backupUser"
+Write-Host "Password: $backupPass`n"
